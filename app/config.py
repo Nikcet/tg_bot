5cbec501753b2ad75b22c65e2c46b1dbce64404c
Nikcet <mstislav.kr@yandex.ru>
2025-07-29 01:29:35 +0300

    init

---
+++ app/config.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from functools import lru_cache
+from typing import Optional
+
+from pydantic_settings import BaseSettings
+
+from app import logger
+
+load_dotenv()
+
+
+class Config(BaseSettings):
+    bot_token: str = os.getenv("BOT_TOKEN", "")
+    webhook_url: Optional[str] = os.getenv("WEBHOOK_URL")
+    webhook_port: int = int(os.getenv("WEBHOOK_PORT", "8443"))
+    debug: bool = os.getenv("DEBUG", "False").lower() == "true"
+    log_file: str = os.getenv("LOG_FILE", "logs.log")
+
+
+@lru_cache
+def get_config() -> Config:
+    logger.info("Loading settings...")
+    return Config()
+
+
+config = get_config()

b150c47302659dfec61e5e8933effdfa8eb226aa
Nikcet <mstislav.kr@yandex.ru>
2025-08-02 13:54:12 +0300

    add new fields to user

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,12 @@
 from datetime import datetime
 from typing import Optional
+from enum import Enum
 from sqlmodel import SQLModel, Field
+
+
+class Language(str, Enum):
+    ENGLISH = "en"
+    RUSSIAN = "ru"
 
 
 class User(SQLModel, table=True):
@@ -9,4 +15,8 @@ class User(SQLModel, table=True):
     last_name: Optional[str] = Field(default=None)
     username: Optional[str] = Field(default=None)
     created_at: datetime = Field(default_factory=datetime.now)
-    last_activity: datetime = Field(default_factory=datetime.now) 
+    last_activity: datetime = Field(default_factory=datetime.now)
+    language: Language = Field(default=Language.ENGLISH)
+    preferences: str = Field(default="")
+    is_premium: bool = Field(default=False)
+    is_admin: bool = Field(default=False)

3e0cfb2a30991c4f22e5d29a3175601b7ad7b1ca
Nikcet <mstislav.kr@yandex.ru>
2025-08-02 13:55:04 +0300

    add new properties to config

--- app/config.py
+++ app/config.py
@@ -12,6 +12,8 @@ load_dotenv()
 
 class Config(BaseSettings):
     bot_token: str = os.getenv("BOT_TOKEN", "")
+    open_api_url: str = os.getenv("OPEN_API_URL", "")
+    open_api_key: str = os.getenv("OPEN_API_KEY", "")
     webhook_url: Optional[str] = os.getenv("WEBHOOK_URL")
     webhook_port: int = int(os.getenv("WEBHOOK_PORT", "8443"))
     debug: bool = os.getenv("DEBUG", "False").lower() == "true"
